a4f6ac069c2d035318fac6494e8868fd3ea3233f
Semenov Sergei <dp120291ssv@gmail.com>
2019-11-07 15:17:01 +0200

    add tests for extract() add assertions with Hamcrest

--- src/test/java/FirstTest.java
+++ src/test/java/FirstTest.java
@@ -1,5 +1,11 @@
 import config.TestConfig;
+import io.restassured.http.Cookie;
+import io.restassured.http.Headers;
+import io.restassured.response.Response;
 import org.testng.annotations.Test;
+
+import java.util.Map;
+
 import static constants.Constants.Actions.SWAPI_GET_PEOPLE;
 import static constants.Constants.Path.SWAPI_PATH;
 import static io.restassured.RestAssured.*;
@@ -44,5 +50,43 @@ public class FirstTest extends TestConfig{
 				.body("count", equalTo(87))
 				.body("results.name[0]", equalTo("Luke Skywalker"))
 				.log().body();
+	}
+
+	@Test
+	public void getAllDataFromSwapi(){
+		Response response =
+			given().spec(requestSpecificationSwapi).log().uri().
+				when().get(SWAPI_PATH).
+				then().extract().response();
+		String jsonResponseAsString = response.asString();
+		System.out.println("jsonResponseAsString-->" + jsonResponseAsString);
+	}
+
+	@Test
+	public void getCookieFromSwapi(){
+		Response response =
+			given().spec(requestSpecificationSwapi).log().uri().
+				when().get(SWAPI_PATH).
+				then().extract().response();
+
+		Map<String, String> allCookie = response.getCookies();
+		System.out.println("cookie--> " + allCookie);
+
+		String someCookie = response.getCookie("__cfduid");
+		System.out.println("Cookie __cfduid --> " + someCookie);
+	}
+
+	@Test
+	public void getHeadersFromSwapi(){
+		Response response =
+			given().spec(requestSpecificationSwapi).log().uri().
+			when().get(SWAPI_PATH).
+			then().extract().response();
+
+		Headers headers = response.getHeaders();
+		System.out.println("headers--> " + headers);
+
+		String contetnType = response.getHeader("Content-Type");
+		System.out.println("Content-Type--> " + contetnType);
 	}
 }
