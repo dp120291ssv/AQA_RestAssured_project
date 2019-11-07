72dade5f3d74e9838642aee4b2d6e073f40a6371
Semenov Sergei <dp120291ssv@gmail.com>
2019-11-07 11:33:23 +0200

    add tests getSwapiPeopleResourcesAssertionExample и getSwapiAllResourcesAssertionExample

--- src/main/java/config/TestConfig.java
+++ src/main/java/config/TestConfig.java
@@ -9,8 +9,13 @@ import org.testng.annotations.BeforeClass;
 import static constants.Constants.RunVariables.path;
 import static constants.Constants.RunVariables.server;
 import static constants.Constants.Server.REQUSTBIN_URL;
+import static constants.Constants.Server.SWAPI_URL;
 
 public class TestConfig {
+
+	protected RequestSpecification requestSpecificationSwapi = new RequestSpecBuilder()
+		.setBaseUri(SWAPI_URL)
+		.build();
 
 	protected RequestSpecification requestSpecificationXml = new RequestSpecBuilder()
 		.addHeader("Content-Type", "application/xml")
--- src/test/java/FirstTest.java
+++ src/test/java/FirstTest.java
@@ -1,7 +1,9 @@
 import config.TestConfig;
 import org.testng.annotations.Test;
 import static constants.Constants.Actions.SWAPI_GET_PEOPLE;
+import static constants.Constants.Path.SWAPI_PATH;
 import static io.restassured.RestAssured.*;
+import static org.hamcrest.CoreMatchers.equalTo;
 
 public class FirstTest extends TestConfig{
 
@@ -14,5 +16,33 @@ public class FirstTest extends TestConfig{
 		then().
 			log().body().
 			statusCode(200);
+	}
+
+	/**
+	 *
+	 * Hamcrest помогает писать тесты.
+	 * Не надо путать его с полнофункциональными фреймворками для тестирования, такими как JUnit.
+	 * Hamcrest – это всего лишь библиотека matcher-ов, которая используется в паре с JUnit или другим
+	 * аналогичным фреймворком для тестирования.
+
+	 Название Hamcrest является анаграммой «matcher». Matcher – это такое выражение,
+	 тестирующее на совпадение с определенным условием.
+	 * */
+
+	@Test
+	public void getSwapiAllResourcesAssertionExample(){
+		given().spec(requestSpecificationSwapi).log().uri().
+		when().get(SWAPI_PATH).
+		then().body("people", equalTo("https://swapi.co/api/people/")).log().body();
+	}
+
+	@Test
+	public void getSwapiPeopleResourcesAssertionExample(){
+		given().spec(requestSpecificationSwapi).log().uri().
+			when().get(SWAPI_PATH + SWAPI_GET_PEOPLE).
+			then()
+				.body("count", equalTo(87))
+				.body("results.name[0]", equalTo("Luke Skywalker"))
+				.log().body();
 	}
 }
